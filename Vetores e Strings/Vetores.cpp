27e05330fc8a5152141dce47b63f98a1ed2953bd
Felipe iori de souza <cfoed2@gmail.com>
2026-09-18 09:46:08 -0300

    Vetores
    Introdução a vetores

---
+++ Vetores e Strings/Vetores.cpp
@@ -0,0 +1,8 @@
+//Vetores
+#include <stdio.h>
+int main(){
+	int numero[5] = {10, 20, 30, 40, 50};
+	int num[] = {10, 20, 30, 40, 50};
+	float x[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	printf("%.2f", x[4]);
+}
